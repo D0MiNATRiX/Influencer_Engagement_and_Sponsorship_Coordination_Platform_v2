892f5b05dd48651efb9eb7e0fad8476aa6d97452
Rajdeep Chatterjee <rajdeep.chatterjee1902@gmail.com>
2024-08-08 15:50:52 +0530

    Login API

--- application/models.py
+++ application/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_security import UserMixin, RoleMixin
 
 db = SQLAlchemy()
 
@@ -8,17 +9,16 @@ class RolesUsers(db.Model):
     user_id = db.Column('user_id', db.Integer(), db.ForeignKey('user.id'))
     role_id = db.Column('role_id', db.Integer(), db.ForeignKey('role.id'))
     
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, unique=False)
     email = db.Column(db.String, unique=True)
     password = db.Column(db.String(255))
     active = db.Column(db.Boolean())
     fs_uniquifier = db.Column(db.String(255), unique=True, nullable=False)
     roles = db.relationship('Role', secondary='roles_users', backref=db.backref('users', lazy='dynamic'))
 
-class Role(db.Model):
-    id = db.Column(db.String, primary_key=True)
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True)
     description = db.Column(db.String(255))
 
--- upload_initial_data.py
+++ upload_initial_data.py
@@ -1,15 +1,17 @@
-from main import app
-from application.models import db,Role
+from main import app, datastore
+from application.models import db, Role
+from flask_security import hash_password
 
 with app.app_context():
     db.create_all()
-    admin = Role(id='admin', name='Admin', description='Admin Description')
-    db.session.add(admin)
-    sponsor = Role(id='sponsor', name='Sponsor', description='Sponsor Description')
-    db.session.add(sponsor)
-    influencer = Role(id='influencer', name='Influencer', description='Influencer Description')
-    db.session.add(influencer)
-    try:
-        db.session.commit()
-    except:
-        pass
+    datastore.find_or_create_role(name="admin", description="User is an admin")
+    datastore.find_or_create_role(name="sponsor", description="User is a sponsor")
+    datastore.find_or_create_role(name="influencer", description="User is an influencer")
+    db.session.commit()
+    if not datastore.find_user(email="admin@email.com"):
+        datastore.create_user(email="admin@email.com", password=hash_password("admin"), roles=["admin"])
+    if not datastore.find_user(email="sponsor1@email.com"):
+        datastore.create_user(email="sponsor1@email.com", password=hash_password("sponsor1"), roles=["sponsor"], active=False)
+    if not datastore.find_user(email="influencer1@email.com"):
+        datastore.create_user(email="influencer1@email.com", password=hash_password("influencer1"), roles=["influencer"])
+    db.session.commit()
